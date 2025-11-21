6287ecf2fddee4eec6aa8adb1e4622c4d50e99a5
UmidaJamshidbekovna <umidarakhimova34@gmail.com>
2025-11-21 15:52:11 +0500

    header section updated ...

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,12 +11,12 @@ export default HomePage
 export async function getServerSideProps(context) {
     // const cookies = parseCookies(context);
     // const accessToken = cookies?.accessToken;
-    return {
-        redirect: {
-            destination: '/user/sms',
-            permanent: false,
-        },
-    };
+    // return {
+    //     redirect: {
+    //         destination: '/user/sms',
+    //         permanent: false,
+    //     },
+    // };
 
     // If user is already logged in, redirect to dashboard
     // if (accessToken) {
